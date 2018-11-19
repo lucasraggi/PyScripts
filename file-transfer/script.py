aafce9911158e2d4834943295970875f09f8e5f4
lucas <lucas.raggi@hotmail.com>
2018-11-18 22:30:58 -0200

    Initial Commit

--- file-transfer/script.py
+++ file-transfer/script.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import sys
+import shutil
 
 def extract_params(line):
     line = line.split("-")
@@ -15,16 +15,28 @@ def extract_params(line):
 with open('input') as pacientes:
     count = 0
     root_path = "E:/HOB/Pacientes - coleta Roberto/"
-    outpath = []
+    outpath = ['H:/',]
     for line in pacientes:
         path_complement, patient_number, first_name, last_name = extract_params(line)
         path = root_path + path_complement
         print(line.split("-"))
+
+        destiny_path = outpath[count] + '/' + path_complement
+        if not os.path.exists(destiny_path):
+            os.makedirs(destiny_path)
+
+        move = False
         for filename in os.listdir(path):
             if filename == first_name:
+                move = True
                 print(filename)
+            if move:
+                origin_path = path + '/' + filename
+                shutil.copy(origin_path, destiny_path)
             if filename == last_name:
+                move = False
                 print(filename)
+
 
         count += 1
         if count == 1:
